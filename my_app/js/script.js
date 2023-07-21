3242af63b3c44b0cc5547281568bb1fc0276d540
Mario-Marchio <mariomarchio1610@gmail.com>
2023-07-21 05:26:45 +0200

    added toggle and line trought on the tasks

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,33 +1,36 @@
-const { createApp }= Vue;
+const { createApp } = Vue;
 
-const app= createApp({
-    data() {
-        return {
-            tasks: [],
-            newTask: ''
-        }
-    },
-    methods: {
-        addTask(){
-            const data = {task: this.newTask };
+const app = createApp({
+  data() {
+    return {
+      tasks: [],
+      newTask: ''
+    };
+  },
+  methods: {
+    addTask() {
+      const data = { task: this.newTask };
 
-            const config ={
-                headers: {'Content-Type': 'multipart/form-data'}
-            }
-            
-            axios.post('http://localhost/php-todo-list-json/api/tasks/', data , config )
-            .then(res =>{
-                this.tasks.push(res.data);
-                this.newTask ='';
-            })
-        }
+      const config = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+
+      axios.post('http://localhost/php-todo-list-json/api/tasks/', data, config)
+        .then(res => {
+          this.tasks.push({ text: res.data, done: false });
+          this.newTask = '';
+        });
     },
-    created(){
-        axios.get('http://localhost/php-todo-list-json/api/tasks/')
-        .then(res=> {
-            this.tasks = res.data;
-        })
+    toggleTask(index) {
+      this.tasks[index].done = !this.tasks[index].done;
     }
-})
+  },
+  created() {
+    axios.get('http://localhost/php-todo-list-json/api/tasks/')
+      .then(res => {
+        this.tasks = res.data.map(task => ({ text: task, done: false }));
+      });
+  }
+});
 
-app.mount('#app')
+app.mount('#app');
